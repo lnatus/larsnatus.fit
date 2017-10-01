2df925817d32bb9757cc545e671094e16422067b
lnatus <l.natus@live.com>
2017-10-01 22:28:16 +0200

    Remove infographics; Add form validation

--- src/scripts/app/app.ts
+++ src/scripts/app/app.ts
@@ -14,4 +14,17 @@ $(document).ready(() => {
   $('#js-lnf-app-nutritionplaner-run').click(()=>{
     np.run()
   })
+
+
+  var $inputInfoEmail = $('#inputInfoEmail')
+  var $btnInfoEmail = $('#btnInfoEmail')
+  var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  $inputInfoEmail.keyup(function() {
+    if(emailRegex.test($inputInfoEmail.val())) {
+      $btnInfoEmail.removeAttr('disabled')
+    } else {
+      $btnInfoEmail.attr('disabled', 'disabled')
+    }
+  });
 })
--- src/scripts/dist/app.js
+++ src/scripts/dist/app.js
@@ -10,6 +10,17 @@ $(document).ready(function () {
     $('#js-lnf-app-nutritionplaner-run').click(function () {
         np.run();
     });
+    var $inputInfoEmail = $('#inputInfoEmail');
+    var $btnInfoEmail = $('#btnInfoEmail');
+    var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    $inputInfoEmail.keyup(function () {
+        if (emailRegex.test($inputInfoEmail.val())) {
+            $btnInfoEmail.removeAttr('disabled');
+        }
+        else {
+            $btnInfoEmail.attr('disabled', 'disabled');
+        }
+    });
 });
 var LNF;
 (function (LNF) {
